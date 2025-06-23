8d2c98d25ef9fa7674e9818687ab2091c83ab8c4
Tom Moreton <18382200+tamoreton@users.noreply.github.com>
2025-06-23 10:52:14 +0100

    remove unneeded dependency

--- commitlint.rules.js
+++ commitlint.rules.js
@@ -1,16 +1,4 @@
-// const configConventional = require("@commitlint/config-conventional");
-
 module.exports = {
-  // rules: {
-  //   "type-enum": [
-  //     2,
-  //     "always",
-  //     [
-  //       ...configConventional.rules["type-enum"][2],
-  //       "deps", // Required for Release Please support
-  //     ],
-  //   ],
-  // },
   rules: {
     "type-enum": [
       2,
